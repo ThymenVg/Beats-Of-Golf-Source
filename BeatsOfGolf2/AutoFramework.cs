a28349f7dee5ff6c5a1a33ade2057c1f90c939ed
thymenvg <thymenvangijsel@hotmail.com>
2024-05-05 14:41:35 +0200

    first commit

---
+++ BeatsOfGolf2/AutoFramework.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace BeatsOfGolf2
+{
+    internal class AutoFramework
+    {
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        const uint SWP_NOSIZE = 0x0001;
+        const uint SWP_NOZORDER = 0x0004;
+        const uint SWP_SHOWWINDOW = 0x0040;
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, UIntPtr dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        const int MOUSEEVENTF_LEFTUP = 0x04;
+        const int MOUSEEVENTF_LEFTDOWN = 0x02;
+
+        public static void OpenProgram(string filePath, int width = 1000, int height = 1000, int x = 0, int y = 0)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo("powershell.exe");
+            startInfo.Arguments = $"-command \"Start-Process '{filePath}'\"";
+            Process.Start(startInfo);
+        }
+
+        public static void RepositionWindow(string processName, int x, int y, int w, int h)
+        {
+            Process[] processes = Process.GetProcessesByName(processName);
+            IntPtr hWnd = processes[0].MainWindowHandle;
+            Thread.Sleep(100);
+            SetForegroundWindow(hWnd);
+            Thread.Sleep(100);
+            SetWindowPos(hWnd, IntPtr.Zero, x, y, w, h, SWP_SHOWWINDOW);
+        }
+
+        public static void DoubbleClickLeftMouseButton(int x, int y)
+        {
+            ClickLeftMouseButton(x, y);
+            Thread.Sleep(100);
+            ClickLeftMouseButton(x, y);
+            Thread.Sleep(100);
+        }
+
+        public static void ClickLeftMouseButton(int x, int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, UIntPtr.Zero);
+        }
+
+        public static void DragLeftMouseButton(int x, int y, int distanceX, int distanceY)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, x, y, 0, UIntPtr.Zero);
+
+            for (int i = 0; i <= distanceX; i++)
+            {
+                Thread.Sleep(1);
+                SetCursorPos(x + i, y);
+            }
+
+            for (int i = 0; i <= distanceY; i++)
+            {
+                Thread.Sleep(1);
+                SetCursorPos(x + distanceX, y + i);
+            }
+
+            mouse_event(MOUSEEVENTF_LEFTUP, x, y, 0, UIntPtr.Zero);
+        }
+    }
+}
---
+++ BeatsOfGolf2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
+
+namespace BeatsOfGolf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines("config.txt");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string[] command = input[i].Split(";");
+                switch (command[0])
+                {
+                    case "1":
+                        OpeningProgram(command);
+                        break;
+                    case "2":
+                        DelayingNextCommand(command);
+                        break;
+                    case "3":
+                        RepositioningWindow(command);
+                        break;
+                    case "4":
+                        DoubbleClicking(command);
+                        break;
+                    case "5":
+                        Clicking(command);
+                        break;
+                    case "6":
+                        Dragging(command);
+                        break;
+                }
+            }
+    
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("\n\n\n\n\n\n |\n |\n |\t© Developed by Thymen Van Gijsel\n |\n |\n\nStartup script finished, you may close this window now . . . ");
+            Console.Read();
+        }
+
+        static void OpeningProgram(string[] command)
+        {
+            Console.WriteLine($"trying to open {command[1]}");
+            AutoFramework.OpenProgram(command[1]);
+            Console.WriteLine("program opened");
+            Thread.Sleep(100);
+        }
+
+        static void DelayingNextCommand(string[] command)
+        {
+            int time = int.Parse(command[1]);
+            Console.WriteLine($"waiting {time}ms");
+            Thread.Sleep(time);
+            Console.WriteLine("timer finished");
+        }
+
+        static void RepositioningWindow(string[] command)
+        {
+            int x = int.Parse(command[2]);
+            int y = int.Parse(command[3]);
+            int w = int.Parse(command[4]);
+            int h = int.Parse(command[5]);
+            AutoFramework.RepositionWindow(command[1], x, y, w, h);
+            Console.WriteLine($"repositioning {command[1]}");
+        }
+
+        static void DoubbleClicking(string[] command)
+        {
+            int x = int.Parse(command[1]);
+            int y = int.Parse(command[2]);
+            AutoFramework.DoubbleClickLeftMouseButton(x, y);
+            Console.WriteLine($"double clicking at x:{x} y:{y}");
+        }
+
+        static void Clicking(string[] command)
+        {
+            int x = int.Parse(command[1]);
+            int y = int.Parse(command[2]);
+            AutoFramework.ClickLeftMouseButton(x, y);
+            Console.WriteLine($"clicking at x:{x} y:{y}");
+        }
+
+        static void Dragging(string[] command)
+        {
+            int x = int.Parse(command[1]);
+            int y = int.Parse(command[2]);
+            int dx = int.Parse(command[3]);
+            int dy = int.Parse(command[4]);
+            AutoFramework.DragLeftMouseButton(x, y, dx, dy);
+            Console.WriteLine($"draggin mouse from x:{x} y:{y} to x:{x+dx} y:{y+dy}");
+        }
+    }
+}
